dcdde6d7932cd97d7d4e5a07d69a692511ac3a75
lajbel <lajbelms@gmail.com>
2024-09-24 20:20:02 -0300

    chore: remove many assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "kaplay/assets/sprites/**",
+                    dest: "sprites/",
+                },
+                {
+                    src: "kaplay/examples/**",
+                    dest: "examples/",
+                },
+            ],
+        }),
+    ],
 });
