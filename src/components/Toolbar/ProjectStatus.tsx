800375de941ca8289a5b39774c1fe350be380bb2
lajbel <lajbelms@gmail.com>
2025-03-25 09:30:27 -0300

    chore: version always visible

--- src/components/Toolbar/ProjectStatus.tsx
+++ src/components/Toolbar/ProjectStatus.tsx
@@ -23,46 +23,49 @@ const ProjectStatus = () => {
         setName(getProject().name);
     }, [getProject().name]);
 
-    if (getProject().isDefault) return;
-
     return (
         <div className="flex flex-row gap-2">
-            <div className="uppercase | badge badge-lg badge-primary">
-                {getProject().mode === "pj" ? "Project" : "Example"}
-            </div>
+            {!getProject().isDefault && (
+                <>
+                    <div className="uppercase | badge badge-lg badge-primary">
+                        {getProject().mode === "pj" ? "Project" : "Example"}
+                    </div>
 
-            <div>
-                {name}
-                {!projectIsSaved(name, getProject().mode) && <span>*</span>}
-            </div>
+                    <div>
+                        {name}
+                        {!projectIsSaved(name, getProject().mode) && (
+                            <span>*</span>
+                        )}
+                    </div>
 
-            <button
-                className="btn btn-ghost btn-xs rounded-sm px-1"
-                onClick={handleEditName}
-                data-tooltip-id="global"
-                data-tooltip-html={`Edit name`}
-                data-tooltip-place="bottom-end"
-            >
-                <img
-                    src={assets.pointer.outlined}
-                    alt="Save Project"
-                    className="h-4"
-                />
-            </button>
-            <button
-                className="btn btn-ghost btn-xs rounded-sm px-1"
-                onClick={handleSaveProject}
-                data-tooltip-id="global"
-                data-tooltip-html={`Save project`}
-                data-tooltip-place="bottom-end"
-            >
-                <img
-                    src={assets.save.outlined}
-                    alt="Save Project"
-                    className="h-4"
-                />
-            </button>
-
+                    <button
+                        className="btn btn-ghost btn-xs rounded-sm px-1"
+                        onClick={handleEditName}
+                        data-tooltip-id="global"
+                        data-tooltip-html={`Edit name`}
+                        data-tooltip-place="bottom-end"
+                    >
+                        <img
+                            src={assets.pointer.outlined}
+                            alt="Save Project"
+                            className="h-4"
+                        />
+                    </button>
+                    <button
+                        className="btn btn-ghost btn-xs rounded-sm px-1"
+                        onClick={handleSaveProject}
+                        data-tooltip-id="global"
+                        data-tooltip-html={`Save project`}
+                        data-tooltip-place="bottom-end"
+                    >
+                        <img
+                            src={assets.save.outlined}
+                            alt="Save Project"
+                            className="h-4"
+                        />
+                    </button>
+                </>
+            )}
             <div className="uppercase | badge badge-lg">
                 {getProject().kaplayVersion}
             </div>
