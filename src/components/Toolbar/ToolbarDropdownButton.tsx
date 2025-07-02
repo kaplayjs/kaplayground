e262c75dfbb27c72eec7336a0fdcf74361de1c73
imaginarny <gencurpeter@gmail.com>
2025-07-02 14:43:04 +0200

    fix: missing forwardRef error

--- src/components/Toolbar/ToolbarDropdownButton.tsx
+++ src/components/Toolbar/ToolbarDropdownButton.tsx
@@ -2,26 +2,23 @@ import {
     DropdownMenuItem,
     DropdownMenuItemProps,
 } from "@radix-ui/react-dropdown-menu";
-import type { FC, PropsWithChildren, RefObject } from "react";
+import { forwardRef, type PropsWithChildren } from "react";
 
-interface ToolbarDropdownButtonProps extends
-    PropsWithChildren<
-        DropdownMenuItemProps
-    >
-{
-    ref?: RefObject<HTMLDivElement>;
-}
+type ToolbarDropdownButtonProps = PropsWithChildren<
+    DropdownMenuItemProps
+>;
 
-export const ToolbarDropdownButton: FC<ToolbarDropdownButtonProps> = ({
-    children,
-    ...props
-}) => {
+export const ToolbarDropdownButton = forwardRef<
+    HTMLDivElement,
+    ToolbarDropdownButtonProps
+>(({ children, ...props }, ref) => {
     return (
         <DropdownMenuItem
+            ref={ref}
             className="btn btn-sm btn-ghost font-normal text-left justify-start pl-1.5 rounded-md px-10 hover:outline-none aria-disabled:btn-disabled aria-disabled:!bg-opacity-0"
             {...props}
         >
             {children}
         </DropdownMenuItem>
     );
-};
+});
